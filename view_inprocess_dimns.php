14ee19cb994757918acfca03368e08842d097b19
sknagesh <sknagesh@gmail.com>
2012-02-03 19:45:04 +0530

    working report?

--- view_inprocess_dimns.php
+++ view_inprocess_dimns.php
@@ -9,53 +9,97 @@ $batchid=$_GET['batchid'];
 $jobq="SELECT DISTINCT(Job_NO) FROM InprocessDimns WHERE Operation_ID='$opid' AND Batch_ID='$batchid';";
 $r = mysql_query($jobq, $cxn) or die(mysql_error($cxn));
 
-	$ipd="<table border=\"1\" cellspacing=\"1\" id=\"inprocesstble\">";
-	$ipd.= "<tr><th>Baloon No</th><th>Dimension Desc</th><th>Basic dimn</th>";
-	$ipd.='<th>Tol Lo</th><th>Tol Upp</th><th>Instrument</th><th>Job No</th>';
+	$z=0;
+	while($i=mysql_fetch_assoc($r))
+	{
+	$jobno[$z]=$i['Job_NO'];	
+	$z++;
+	}
+//print("<br>jobno");
+//print_r($jobno);
+
 
 	$jobq="SELECT Job_NO FROM InprocessDimns WHERE Operation_ID='$opid' AND Batch_ID='$batchid';";
 	$r = mysql_query($jobq, $cxn) or die(mysql_error($cxn));
-while($i=mysql_fetch_assoc($r))
-{
-$ipd.="<th>Job No $i[Job_NO]</th>";	
-}
 
-$ipd.='</tr>';
+	$qry="SELECT ip.Operation_ID, Basic_Dimn,Dimn_Desc,Tol_Lower,Tol_Upper,ip.Instrument_ID,Instrument_Desc,";
+	$qry.="Baloon_NO FROM InProcess as ip ";
+	$qry.="INNER JOIN Instrument AS inst ON inst.Instrument_ID=ip.Instrument_ID ";
+	$qry.="INNER JOIN Operation AS op ON ip.Operation_ID=op.Operation_ID ";
+	$qry.="INNER JOIN Components AS comp ON op.Drawing_ID=comp.Drawing_ID ";
+	$qry.="WHERE ip.Operation_ID='$opid' ORDER BY Baloon_NO;";
+//print("<br>$qry");
+	$j=0;
+	$resa = mysql_query($qry, $cxn) or die(mysql_error($cxn));
+	while ($row = mysql_fetch_assoc($resa))
+        		{
+	$lrows[$j]=array($row['Baloon_NO'],$row['Dimn_Desc'],$row['Basic_Dimn'],$row['Tol_Lower'].'/'.$row['Tol_Upper'],$row['Instrument_Desc']);		
+	$j++;
+		        }
+//print("<br>lrows");
+//print_r($lrows);
 
-
-	$qry="SELECT ipd.Operation_ID, ipd.Batch_ID, IP_ID,Inspector_ID, Basic_Dimn,Dimn_Desc,Tol_Lower,Tol_Upper,ip.Instrument_ID,Instrument_Desc,Text_Field,Prod_Dimn,";
-	$qry.="Baloon_NO,Job_Date,Job_NO,Dimn_Measured,Remarks FROM InprocessDimns as ipd ";
-	$qry.="INNER JOIN InProcess AS ip ON ipd.IP_ID=ip.InProcess_ID ";
+  	$qry="SELECT Baloon_NO,Group_concat(Dimn_Measured order by Job_NO) as dimensions FROM InprocessDimns as ipd ";
+	$qry.="LEFT JOIN InProcess AS ip ON ipd.IP_ID=ip.InProcess_ID ";
 	$qry.="INNER JOIN Batch_NO AS bno ON ipd.Batch_ID=bno.Batch_ID ";
 	$qry.="INNER JOIN Instrument AS inst ON inst.Instrument_ID=ip.Instrument_ID ";
 	$qry.="INNER JOIN Operation AS op ON ipd.Operation_ID=op.Operation_ID ";
 	$qry.="INNER JOIN Components AS comp ON op.Drawing_ID=comp.Drawing_ID ";
-	$qry.="WHERE ipd.Operation_ID='$opid' AND ipd.Batch_ID='$batchid';";
+	$qry.="WHERE ipd.Operation_ID='$opid' AND ipd.Batch_ID='$batchid' GROUP BY Baloon_NO;";
+	$job = mysql_query($qry, $cxn) or die(mysql_error($cxn));
+$z=0;
+	while($jobv=mysql_fetch_assoc($job))
+	{
+				$rrow[$z]=explode(",",$jobv['dimensions']);
+//			print("$jobv[Baloon_NO] - $jobv[dimensions]<br>");
+	$z+=1;
+	}
 
-	$resa = mysql_query($qry, $cxn) or die(mysql_error($cxn));
-	while ($row = mysql_fetch_assoc($resa))
-        		{
-	$ipd.="<tr><td>$row[Baloon_NO]</td><td>$row[Dimn_Desc]</td><td>$row[Basic_Dimn]</td>";
-	$ipd.="<td>$row[Tol_Lower]</td><td>$row[Tol_Upper]</td><td>$row[Instrument_Desc]</td><td>$row[Job_NO]</td>";
-		if($row['Text_Field']==0)
+//print("<br>rrows<br>");
+//print_r(array_values($rrow));
+
+	$ipd="<table border=\"1\" cellspacing=\"1\" id=\"inprocesstble\">";
+	$ipd.= "<tr><th>Baloon No</th><th>Dimension Desc</th><th>Basic dimn</th>";
+	$ipd.='<th>Tolerance</th><th>Instrument</th>';
+	$z=0;
+	while($z<count($jobno))
+	{
+	$ipd.="<th>$jobno[$z]</th>";
+	$z++;
+	}
+	$ipd.='</tr>';  
+
+
+
+	$z=0;
+	while($z<count($lrows))
+	{
+		$ipd.="<tr>";
+		foreach($lrows[$z] as $x)
 		{
-			if($row['Dimn_Measured']=="1")
-				{
-				$ipd.="<td>With In Tolerance ";
-				}else
-				{
-				$ipd.="<td>Out Of Tolerance ";
-				}
-		}else
-				{
-				$ipd.="<td>$row[Dimn_Measured]";
-				}
-	$ipd.="$row[Remarks]</td></tr>";        				
-        			
-		        }
-		$ipd.='</table>';
+		$ipd.="<td>$x</td>";
+		}
+
+		foreach($rrow[$z] as $x)
+		{
+		$ipd.="<td>$x</td>";
+		}
+
+	
+	$ipd.="</tr>";
+	$z++;
+	}
 
 
+
+
+
+
+
+  
+  		$ipd.='</table>';
+  
+        				
 
 
 
