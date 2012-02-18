780f1a21e248591b2588de56fa0d62f320824d5e
sknagesh <sknagesh@gmail.com>
2012-02-18 11:21:09 +0530

    changed view dimensions

--- view_inprocess_dimns.php
+++ view_inprocess_dimns.php
@@ -12,7 +12,7 @@ $r = mysql_query($jobq, $cxn) or die(mysql_error($cxn));
 	$z=0;
 	while($i=mysql_fetch_assoc($r))
 	{
-	$jobno[$z]=$i['Job_NO'];	
+	$jobno[$z]=$i['Job_NO'];  //store unique job nos to display in headding	
 	$z++;
 	}
 //print("<br>jobno");
@@ -27,11 +27,11 @@ $r = mysql_query($jobq, $cxn) or die(mysql_error($cxn));
 	$qry.="INNER JOIN Instrument AS inst ON inst.Instrument_ID=ip.Instrument_ID ";
 	$qry.="INNER JOIN Operation AS op ON ip.Operation_ID=op.Operation_ID ";
 	$qry.="INNER JOIN Components AS comp ON op.Drawing_ID=comp.Drawing_ID ";
-	$qry.="WHERE ip.Operation_ID='$opid' ORDER BY Baloon_NO;";
+	$qry.="WHERE ip.Operation_ID='$opid' ORDER BY Baloon_NO ASC;";
 //print("<br>$qry");
 	$j=0;
 	$resa = mysql_query($qry, $cxn) or die(mysql_error($cxn));
-	while ($row = mysql_fetch_assoc($resa))
+	while ($row = mysql_fetch_assoc($resa))  //get all dimensions for thi operation and store them in an array
         		{
 	$lrows[$j]=array($row['Baloon_NO'],$row['Dimn_Desc'],$row['Basic_Dimn'],$row['Tol_Lower'].'/'.$row['Tol_Upper'],$row['Instrument_Desc']);		
 	$j++;
@@ -39,68 +39,78 @@ $r = mysql_query($jobq, $cxn) or die(mysql_error($cxn));
 //print("<br>lrows");
 //print_r($lrows);
 
-  	$qry="SELECT Baloon_NO,Group_concat(Dimn_Measured order by Job_NO) as dimensions FROM InprocessDimns as ipd ";
-	$qry.="LEFT JOIN InProcess AS ip ON ipd.IP_ID=ip.InProcess_ID ";
-	$qry.="INNER JOIN Batch_NO AS bno ON ipd.Batch_ID=bno.Batch_ID ";
-	$qry.="INNER JOIN Instrument AS inst ON inst.Instrument_ID=ip.Instrument_ID ";
-	$qry.="INNER JOIN Operation AS op ON ipd.Operation_ID=op.Operation_ID ";
-	$qry.="INNER JOIN Components AS comp ON op.Drawing_ID=comp.Drawing_ID ";
-	$qry.="WHERE ipd.Operation_ID='$opid' AND ipd.Batch_ID='$batchid' GROUP BY Baloon_NO;";
-	$job = mysql_query($qry, $cxn) or die(mysql_error($cxn));
-$z=0;
-	while($jobv=mysql_fetch_assoc($job))
+$jobq="SELECT DISTINCT(Job_NO) FROM InprocessDimns WHERE Operation_ID='$opid' AND Batch_ID='$batchid';";
+$r = mysql_query($jobq, $cxn) or die(mysql_error($cxn));
+
+	$z=0;
+	while($i=mysql_fetch_assoc($r))  //loop through each job inspected
 	{
-				$rrow[$z]=explode(",",$jobv['dimensions']);
-//			print("$jobv[Baloon_NO] - $jobv[dimensions]<br>");
+	$qry="SELECT InProcess_ID, InprocessDimns.Operation_ID, 
+					Basic_Dimn, InProcessDimn_ID, IP_ID, Batch_ID,
+					Job_NO, Dimn_Measured FROM InProcess
+					LEFT OUTER JOIN InprocessDimns
+					ON InProcess.InProcess_ID = InprocessDimns.IP_ID AND InprocessDimns.Job_NO='$i[Job_NO]'  AND Batch_ID='$batchid'
+					WHERE InProcess.Operation_ID = '$opid' ORDER BY Baloon_NO ASC";
+//		print($qry);
+		$res = mysql_query($qry, $cxn) or die(mysql_error($cxn));
+		$x=0;
+		while($row=mysql_fetch_assoc($res))  //for each job get dimensions measured and store it in an array
+		{
+			$rrow[$z][$x]=$row['Dimn_Measured'];
+			$x+=1;
+		}
+//	$jobno[$z]=$i['Job_NO'];	
 	$z+=1;
 	}
 
 //print("<br>rrows<br>");
-//print_r(array_values($rrow));
+//print_r($rrow);
+
+
 if(!isSet($rrow))
-{
-	$ipd="<br>No Dimensions entered for this operation for this batch number";
+{ //if no jobs are measured for this operation
+	print("<br>No Dimensions entered for this operation for this batch number");
 }
-else {
-	$ipd="<table border=\"1\" cellspacing=\"1\" id=\"inprocesstble\">";
-	$ipd.= "<tr><th>Baloon No</th><th>Dimension Desc</th><th>Basic dimn</th>";
-	$ipd.='<th>Tolerance</th><th>Instrument</th>';
-	$z=0;
-	while($z<count($jobno))
-	{
-	$ipd.="<th>$jobno[$z]</th>";
-	$z++;
-	}
-	$ipd.='</tr>';  
-
-
-
-	$z=0;
-	while($z<count($lrows))
-	{
-		$ipd.="<tr>";
-		foreach($lrows[$z] as $x)
+else { 
+		print("<table border=\"1\" cellspacing=\"1\" id=\"inprocesstble\">");
+		print("<tr><th>Baloon No</th><th>Dimension Desc</th><th>Basic dimn</th>");
+		print("<th>Tolerance</th><th>Instrument</th>");
+		$z=0;
+		while($z<count($jobno))  //append job nos to first row heading
 		{
-		$ipd.="<td>$x</td>";
+		print("<th>$jobno[$z]</th>");
+		$z++;
+		}
+		print("</tr>");  
+
+		$z=0;
+		while($z<count($lrows))  //while no of dimensions defined for this operation
+		{
+			print("<tr>");
+			foreach($lrows[$z] as $x) //print dimension details
+			{
+			print("<td>$x</td>");
+			$x+=1;
+			}
+			$s=0;
+			while($s<count($jobno))  //print relavant dimensions measured
+			{
+			print("<td>");
+			print($rrow[$s][$z]);
+			print("</td>");
+			$s+=1;
+			}
+			print("</tr>");   //end of each row
+		$z++;		
 		}
 
-		foreach($rrow[$z] as $x)
-		{
-		$ipd.="<td>$x</td>";
-		}
-
-	
-	$ipd.="</tr>";
-	$z++;
-	}
-  		$ipd.='</table>';
+  		print("</table>");
   
   }        				
 
 
 
 
-	echo( $ipd );
-	
+
 	
 ?>
